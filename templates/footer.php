b0b52f936999f6d909c153e48eda20cd8c55da75
toniG <cf21toni.gomila@iesjoandaustria.org>
2022-11-03 16:20:31 +0100

    Plantilles: Actualizades plantilles head header i footer

---
+++ templates/footer.php
@@ -0,0 +1,21 @@
+    
+    <footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
+            <span class="text-sm text-gray-500 sm:text-center">© 2022 <a href="#" class="hover:underline">ToniGomila</a>. All Rights Reserved.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,3 @@
+<header class="bg-gradient-to-r from-[#614385] to-[#516395] w-full py-5 mb-10 px-20">
+        <h2 class="text-white text-xl container mx-auto">Projecte m6 m7</h2>
+</header>
